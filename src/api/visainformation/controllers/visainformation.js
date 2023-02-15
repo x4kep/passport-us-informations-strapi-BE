c6ca4517a773b1ebe606fcdccf1bb24ce208b2ae
dusan.veselinovic.dev@gmail.com <dusan.veselinovic@automed.io-at-347013622989>
2023-02-15 12:56:27 +0100

    Feat: Upload file

--- src/api/visainformation/controllers/visainformation.js
+++ src/api/visainformation/controllers/visainformation.js
@@ -1,9 +1,34 @@
-'use strict';
+"use strict";
 
 /**
  * visainformation controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::visainformation.visainformation');
+module.exports = createCoreController(
+  "api::visainformation.visainformation",
+  () => ({
+    async create(ctx) {
+      const { data } = ctx.request.body;
+      const files = ctx.request.files;
+      const parsedData = JSON.parse(data);
+
+      // Documentation: https://docs.strapi.io/developer-docs/latest/developer-resources/database-apis-reference/entity-service/crud.html#findmany
+      const entry = await strapi.entityService.create(
+        "api::visainformation.visainformation",
+        {
+          data: {
+            ...parsedData,
+          },
+          files: {
+            photo: files["files.photo"],
+          },
+        }
+      );
+
+      ctx.send(entry);
+      return entry;
+    },
+  })
+);
